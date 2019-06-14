1e19675d53583ede82cac02b3e89ac9763eae270
Alex Velasquez <jtinnesco@gmail.com>
2019-06-14 23:31:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,55 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets 
+  attr_reader :species 
+  
+  @@all = [] 
+  
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end 
+  
+  def buy_fish(fish)
+    @pets[:fishes] << Fish.new(fish)
+  end
+  
+  def buy_cat(cat)
+    @pets[:cats] << Cat.new(cat)
+  end
+  
+    def buy_dog(dog)
+    @pets[:dogs] << Dog.new(dog)
+  end
+  
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+  
+  def say_species
+    p "I am a #{@species}."
+  end
+  
+  def sell_pets
+    @pets.each do |pet|
+      pet.mood = "nervous"
+    end
+  end
+  
+  
+  
+  
 end
